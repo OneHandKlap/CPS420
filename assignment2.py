4b15388e038f10103de55bd9a449a7c2e494e883
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2020-03-07 10:25:12 -0500

    attempt backtrack, stuck in loop trying same paths over and over again

--- assignment2.py
+++ assignment2.py
@@ -24,25 +24,43 @@ def getHamiltonian(graph,start,end):
 
 
     def hamiltonianPath(walk, toVisit, unvisited, backFrom=None):
+        print("Walk: "+str(walk)+" toVisit: "+str(toVisit)+" unvisited: "+str(unvisited)+" backfrom: "+str(backFrom))
 
+        #base case success
         if unvisited==[] and len(walk)==max(walk)+1:
             return walk
-        
+        #base case fail
         if toVisit==[] and len(unvisited)>0:
             return None
+        
 
-        x= toVisit.pop(0)
+        #if the next vertex to travel to is connected, lets go there, else we have to backtrack
+        if toVisit[0] in adjacencies[walk[len(walk)-1]]:
+            x= toVisit.pop(0)
+            if x==backFrom:
+                toVisit.append(x)
+                return (hamiltonianPath(walk,toVisit,unvisited))
+        else:
+            backFrom=walk.pop()
+            toVisit.append(backFrom)
+            unvisited.append(backFrom)
+            print("Dead end, backtracking")
+            return(hamiltonianPath(walk, toVisit,unvisited,backFrom))
+
+        #if we havent been here yet, start looking for new paths
         if x in unvisited:
             unvisited.remove(x)
+        #if this is the last vertex we are done
         elif unvisited==[]:
             return walk
+        #if we have been here before, discard and check the next item in the toVisit list
         else:
-            #backtrack
-            print(walk)
-            return("Dead end")
+            return(hamiltonianPath(walk, toVisit,unvisited,backFrom))
         walk.append(x)
+        if x in toVisit:
+            toVisit=list(filter(lambda a: a!=x,toVisit))
         for option in adjacencies[x]:
-            if option in unvisited and option not in walk:
+            if option in unvisited and option not in walk and option != backFrom:
                 toVisit.insert(0,option)
         return (hamiltonianPath(walk, toVisit,unvisited))
 
